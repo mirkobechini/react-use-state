cff5a786691f34200c137bbb750ebfe2dbc34576
mirkobechini <mirkobechini@gmail.com>
2025-09-30 21:58:56 +0200

    Fix: temporary removed AccordionHeader and AccordionCard

--- src/components/AppMain/AccordionCard/AccordionCard.jsx
+++ src/components/AppMain/AccordionCard/AccordionCard.jsx
@@ -1,14 +1,7 @@
 export default function AccordionCard({ item }) {
     return (
         <>
-            <div className="row">
-                <div className="card my-5">
-                    <div className="card-body">
-                        <h2>{item.title}</h2>
-                        <p> {item.description} </p>
-                    </div>
-                </div>
-            </div>
+            
         </>
     )
 }
--- src/components/AppMain/AccordionCard/AppAccordion.jsx
+++ src/components/AppMain/AccordionCard/AppAccordion.jsx
@@ -5,8 +5,23 @@ export default function AppAccordion({ items }) {
     return (
         <>
             <div className="container py-5">
-                <AccordionHeader items={items} />
-                <AccordionCard item={items[1]} />
+                <div className="row row-cols-auto">
+                    {items.map(item =>
+                        <div className="col ps-0" key={item.id}>
+                            <button className="btn btn-primary" >
+                                {item.title}
+                            </button>
+                        </div>
+                    )}
+                </div>
+                <div className="row">
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h2>{items[0].title}</h2>
+                            <p> {items[0].description} </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
