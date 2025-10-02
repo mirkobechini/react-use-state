1888095ad2703348ed0c9c8fed53b890c0574492
mirkobechini <mirkobechini@gmail.com>
2025-10-02 09:01:21 +0200

    Add: onClick, handleClick and useState to AppAccordion

--- src/components/AppMain/AccordionCard/AppAccordion.jsx
+++ src/components/AppMain/AccordionCard/AppAccordion.jsx
@@ -1,24 +1,37 @@
+import { useState } from 'react'
+
 import AccordionHeader from './AccordionHeader'
 import AccordionCard from './AccordionCard'
 
 export default function AppAccordion({ items }) {
+    
+     const [currentLanguage, setCurrentLanguage] = useState(1)
+
+    function handleClick(e){
+        e.target.className +=' btn-warning'
+        const currentlyClicked = Number(e.target.getAttribute('data-id'))
+        setCurrentLanguage(currentlyClicked)
+        console.log('ciao', e);
+        
+    }
+    
     return (
         <>
             <div className="container py-5">
                 <div className="row row-cols-auto">
                     {items.map(item =>
                         <div className="col ps-0" key={item.id}>
-                            <button className="btn btn-primary" >
+                            <button className="btn btn-primary" data-id= {item.id} onClick={handleClick} >
                                 {item.title}
                             </button>
                         </div>
                     )}
                 </div>
                 <div className="row">
-                    <div className="card my-5">
+                    <div className="card my-5 ps-0">
                         <div className="card-body">
-                            <h2>{items[0].title}</h2>
-                            <p> {items[0].description} </p>
+                            <h2>{items[currentLanguage-1].title}</h2>
+                            <p> {items[currentLanguage-1].description} </p>
                         </div>
                     </div>
                 </div>
