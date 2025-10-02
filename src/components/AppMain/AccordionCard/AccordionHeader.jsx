f50fa66fa42acdf6f670a7f0feda12699d8931b8
mirkobechini <mirkobechini@gmail.com>
2025-10-02 09:18:36 +0200

    Add: divide AppAccordion in 2 components

--- src/components/AppMain/AccordionCard/AccordionHeader.jsx
+++ src/components/AppMain/AccordionCard/AccordionHeader.jsx
@@ -1,7 +1,22 @@
-export default function AccordionHeader({ items }) {
+export default function AccordionHeader({items, language, setCurrentLanguage}) {
+
+    function handleClick(e){
+        const currentlyClicked = Number(e.target.getAttribute('data-id'))
+        setCurrentLanguage(currentlyClicked)
+        
+    }
+
     return (
         <>
-            
+            <div className="row row-cols-auto">
+                    {items.map(item =>
+                        <div className="col ps-0" key = {item.id}>
+                            <button className= {`btn ${language === item.id ? 'btn-warning' : 'btn-primary'}`} data-id= {item.id} onClick={handleClick} >
+                                {item.title}
+                            </button>
+                        </div>
+                    )}
+                </div>
         </>
     )
 }
--- src/components/AppMain/AccordionCard/AppAccordion.jsx
+++ src/components/AppMain/AccordionCard/AppAccordion.jsx
@@ -5,34 +5,13 @@ import AccordionCard from './AccordionCard'
 
 export default function AppAccordion({ items }) {
     
-     const [currentLanguage, setCurrentLanguage] = useState(1)
-
-    function handleClick(e){
-        const currentlyClicked = Number(e.target.getAttribute('data-id'))
-        setCurrentLanguage(currentlyClicked)
-        
-    }
+    const [currentLanguage, setCurrentLanguage] = useState(1)
     
     return (
         <>
             <div className="container py-5">
-                <div className="row row-cols-auto">
-                    {items.map(item =>
-                        <div className="col ps-0" key={item.id}>
-                            <button className= {`btn ${currentLanguage === item.id ? 'btn-warning' : 'btn-primary'}`} data-id= {item.id} onClick={handleClick} >
-                                {item.title}
-                            </button>
-                        </div>
-                    )}
-                </div>
-                <div className="row">
-                    <div className="card my-5 ps-0">
-                        <div className="card-body">
-                            <h2>{items[currentLanguage-1].title}</h2>
-                            <p> {items[currentLanguage-1].description} </p>
-                        </div>
-                    </div>
-                </div>
+                <AccordionHeader items = {items} language = {currentLanguage} setCurrentLanguage = {setCurrentLanguage}/>
+                <AccordionCard item = {items[currentLanguage-1]} />
             </div>
         </>
     )
